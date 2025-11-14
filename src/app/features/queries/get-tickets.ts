7580b4f932385e603b69e730fc0fb96d7ffe473d
Mohamed-A-Yehia <mohamed.a.yehia9@gmail.com>
2025-11-14 04:03:58 +0200

    install: prisma & adding some helpers scripts - fetch the data from the database (supabase)

--- src/app/features/queries/get-tickets.ts
+++ src/app/features/queries/get-tickets.ts
@@ -1,10 +1,9 @@
-import { Ticket } from "@/app/features/ticket/types";
-import { initialTickets } from "@/data";
+import { prisma } from "@/lib/prisma";
 
-export const getTickets = async (): Promise<Ticket[]> => {
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-
-  return new Promise((resolve) => {
-    resolve(initialTickets);
+export const getTickets = async () => {
+  return await prisma.ticket.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
   });
 };
