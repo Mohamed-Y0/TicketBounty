77c17c1e98095ae472a22518494f0234b7289d46
Mohamed-A-Yehia <mohamed.a.yehia9@gmail.com>
2025-11-15 01:26:26 +0200

    impelement create ticket form and connect it with server action ceate-ticket.ts

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,7 +1,15 @@
 import { Suspense } from "react";
+import TicketCreateForm from "@/app/features/ticket/components/ticket-create-form";
 import TicketList from "@/app/features/ticket/components/ticket-list";
 import Heading from "@/components/Heading";
 import Spinner from "@/components/Spinner";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 const TicketsPage = () => {
   return (
@@ -10,6 +18,16 @@ const TicketsPage = () => {
         title={"Tickets"}
         description={"All your tickets at one place"}
       />
+
+      <Card className="w-full max-w-[420px] self-center">
+        <CardHeader>
+          <CardTitle>Create Ticket</CardTitle>
+          <CardDescription>A new ticket will be created</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <TicketCreateForm />
+        </CardContent>
+      </Card>
 
       <Suspense fallback={<Spinner />}>
         <TicketList />
