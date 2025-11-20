cdf178f912b9a9494a56e8200370ec04ca0a2c6f
Mohamed-A-Yehia <mohamed.a.yehia9@gmail.com>
2025-11-20 06:26:22 +0200

    fix the data that inside seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,33 +5,32 @@ const prisma = new PrismaClient();
 const tickets = [
   {
     title: "Ticket 1",
-    content: "This is the first ticket From The Database.",
+    content: "First ticket from DB.",
     status: "DONE" as const,
+    deadline: new Date().toISOString().split("T")[0],
+    bounty: 499,
   },
   {
     title: "Ticket 2",
-    content: "This is the Second ticket From The Database.",
+    content: "Second ticket from DB.",
     status: "OPEN" as const,
+    deadline: new Date().toISOString().split("T")[0],
+    bounty: 399,
   },
   {
     title: "Ticket 3",
-    content: "This is the Third ticket From The Database.",
+    content: "Third ticket from DB.",
     status: "IN_PROGRESS" as const,
+    deadline: new Date().toISOString().split("T")[0],
+    bounty: 599,
   },
 ];
-
 const seed = async () => {
-  const t0 = performance.now();
-  console.log("DB Seed: Started...");
-
   await prisma.ticket.deleteMany();
 
   await prisma.ticket.createMany({
     data: tickets,
   });
-
-  const t1 = performance.now();
-  console.log(`DB Seed: Finished ${t1 - t0}ms`);
 };
 
 seed();
