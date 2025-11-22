b8beb40a5fc61c6d3edfa5c2b2314ef925c6067e
Mohamed-A-Yehia <mohamed.a.yehia9@gmail.com>
2025-11-22 04:06:27 +0200

    protecting the api to prevent the users to make operations on other users tickets

--- src/features/ticket/actions/delete-ticket.ts
+++ src/features/ticket/actions/delete-ticket.ts
@@ -3,12 +3,29 @@
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { setCookieByKey } from "@/actions/cookies";
-import { fromErroToActionState } from "@/components/form/utils/to-action-state";
+import {
+  fromErroToActionState,
+  toActionState,
+} from "@/components/form/utils/to-action-state";
+import { getAuthOrRedirect } from "@/features/auth/queries/getAuthOrRedirect";
+import { isOwner } from "@/features/auth/utils/is-owner";
 import { prisma } from "@/lib/prisma";
 import { ticketsPath } from "@/paths";
 
 export const deleteTicket = async (id: string) => {
+  const { user } = await getAuthOrRedirect();
+
   try {
+    if (id) {
+      const ticket = await prisma.ticket.findUnique({
+        where: { id },
+      });
+
+      if (!ticket || !isOwner(user, ticket)) {
+        return toActionState("ERROR", "Not Authorized.");
+      }
+    }
+
     await prisma.ticket.delete({
       where: { id },
     });
--- src/features/ticket/actions/update-ticket-status.ts
+++ src/features/ticket/actions/update-ticket-status.ts
@@ -6,11 +6,25 @@ import {
   fromErroToActionState,
   toActionState,
 } from "@/components/form/utils/to-action-state";
+import { getAuthOrRedirect } from "@/features/auth/queries/getAuthOrRedirect";
+import { isOwner } from "@/features/auth/utils/is-owner";
 import { prisma } from "@/lib/prisma";
 import { ticketsPath } from "@/paths";
 
 export const updateTicketStatus = async (id: string, status: TicketStatus) => {
+  const { user } = await getAuthOrRedirect();
+
   try {
+    if (id) {
+      const ticket = await prisma.ticket.findUnique({
+        where: { id },
+      });
+
+      if (!ticket || !isOwner(user, ticket)) {
+        return toActionState("ERROR", "Not Authorized.");
+      }
+    }
+
     await prisma.ticket.update({
       where: {
         id,
