a98175157b2d76cec91e5f3d08096455e80e7703
Slayver <mohamed.a.yehia9@gmail.com>
2025-12-01 07:11:45 +0200

    finish: creating comment form and connect it with the database

--- src/features/comment/components/comments.tsx
+++ src/features/comment/components/comments.tsx
@@ -1,3 +1,5 @@
+import { CardCompact } from "@/components/card-compact";
+import CommentCreateForm from "@/features/comment/components/comment-create-form";
 import CommentItem from "@/features/comment/components/comment-item";
 import { getComments } from "@/features/comment/queries/get-comments";
 
@@ -9,11 +11,18 @@ const Comments = async ({ ticketId }: CommentsProps) => {
   const comments = await getComments(ticketId);
 
   return (
-    <div className="ml-8 flex flex-col gap-y-2">
-      {comments.map((comment) => (
-        <CommentItem key={comment.id} comment={comment} />
-      ))}
-    </div>
+    <>
+      <CardCompact
+        title="Create Comment"
+        description="A new comment will be created"
+        content={<CommentCreateForm ticketId={ticketId} />}
+      />
+      <div className="ml-8 flex flex-col gap-y-2">
+        {comments.map((comment) => (
+          <CommentItem key={comment.id} comment={comment} />
+        ))}
+      </div>
+    </>
   );
 };
 
