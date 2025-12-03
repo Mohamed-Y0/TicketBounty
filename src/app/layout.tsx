74aa8ffc0b0f0cd891100eb411769ec31316abb5
Slayver <mohamed.a.yehia9@gmail.com>
2025-12-03 08:01:51 +0200

    refactor comments section using react-query and creating an infinite scroll

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import { NuqsAdapter } from "nuqs/adapters/next/app";
 import Header from "@/app/_navigation/Header";
 import Sidebar from "@/app/_navigation/sidebar/components/sidebar";
+import ReactQueryProviders from "@/app/_providers/react-query/react-query-provider";
 import { RedirectToast } from "@/components/redirect-toast";
 import { ThemeProvider } from "@/components/theme/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
@@ -34,14 +35,16 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ThemeProvider>
-          <Header />
-          <div className="flex h-screen border-collapse overflow-hidden">
-            <Sidebar />
-            <main className="bg-secondary/20 flex min-h-screen flex-1 flex-col overflow-auto overflow-x-hidden px-8 py-24">
-              <NuqsAdapter>{children}</NuqsAdapter>
-            </main>
-          </div>
-          <Toaster expand />
+          <ReactQueryProviders>
+            <Header />
+            <div className="flex h-screen border-collapse overflow-hidden">
+              <Sidebar />
+              <main className="bg-secondary/20 flex min-h-screen flex-1 flex-col overflow-auto overflow-x-hidden px-8 py-24">
+                <NuqsAdapter>{children}</NuqsAdapter>
+              </main>
+            </div>
+            <Toaster expand />
+          </ReactQueryProviders>
           <RedirectToast />
         </ThemeProvider>
       </body>
